242c08230799eec52c56d90bfcdfbddcd35457da
Serena Bertozzi <serenabertozzi@protonmail.com>
2021-12-05 19:59:55 +0000

    Complete card component

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,12 +6,6 @@ import WorkspaceCard from "./WorkspaceCard";
 const MainPage = (props) => {
   const [currentData, loading] = WorkspaceDataAPI();
 
-  const WorkspaceInfo = () => {
-    currentData.workspaces.map((workspace) => {
-      return <WorkspaceCard workspace={workspace} />;
-    });
-  };
-
   return (
     <Container>
       <Row>
@@ -22,7 +16,7 @@ const MainPage = (props) => {
           </div>
         </Col>
       </Row>
-      <Row>
+      <Row className="justify-content-center">
         {loading
           ? ""
           : currentData.workspaces.map((workspace) => {
@@ -34,8 +28,3 @@ const MainPage = (props) => {
 };
 
 export default MainPage;
-
-// {currentData.workspaces.map((workspace) => {
-//   return <h1>{workspace.name}</h1>;
-//   // <WorkspaceCard workspace={workspace} />;
-// })}
--- src/components/WorkspaceCard.js
+++ src/components/WorkspaceCard.js
@@ -1,15 +1,62 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const WorkspaceCard = (props) => {
   return (
-    <Col xs={12} lg={4}>
-      <Card style={{ width: "24rem" }} className="my-2 mx-2">
-        <Card.Img variant="top" src={props.workspace.image} />
+    <Col xs={12} md={6} xl={4}>
+      <Card className="custom-card my-md-4">
+        <Card.Img
+          variant="top"
+          src={
+            props.workspace.image === undefined
+              ? "https://steadway.de/wp-content/uploads/2020/10/TOG_top.jpg"
+              : props.workspace.image
+          }
+        />
         <Card.Body>
-          <Card.Title className="c-title">{props.workspace.name}</Card.Title>
-          <Card.Text>{props.workspace.address}</Card.Text>
+          <Card.Title className="custom-title">
+            {props.workspace.name}
+          </Card.Title>
+          <Card.Text style={{ fontSize: "0.8em" }}>
+            {props.workspace.address}
+          </Card.Text>
+          <hr></hr>
+          <Row className="justify-content-between">
+            <Col xs={4} className="d-flex align-content-center flex-column">
+              <p className="fw-bold">Rooms / Size</p>
+              <div>
+                {props.workspace.rooms.map((room) => {
+                  return (
+                    <div className="d-flex justify-content-between">
+                      <p className="fw-bold">{room.amount}</p>
+                      <p className="text-capitalize">{room.size}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </Col>
+            <Col xs={4} className="d-flex align-content-center flex-column">
+              <p className="fw-bold">Capacity</p>
+              {props.workspace.rooms.map((room) => {
+                return (
+                  <div className="d-flex justify-content-center">
+                    <p>{room.capacity}</p>
+                  </div>
+                );
+              })}
+            </Col>
+            <Col xs={4} className="d-flex justify-content-center flex-column">
+              <p className="fw-bold">Price</p>
+              {props.workspace.rooms.map((room) => {
+                return (
+                  <div className="d-flex justify-content-start">
+                    <p>£{room.price}/h</p>
+                  </div>
+                );
+              })}
+            </Col>
+          </Row>
         </Card.Body>
       </Card>
     </Col>
